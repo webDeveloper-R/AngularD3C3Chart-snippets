ff39ebd465c2db096a4107ccfce18e935007f9ed
webDeveloper-R <webdeveloper0423@gmail.com>
2016-10-10 11:23:09 +0300

    c3 chart --------------05

--- Javascript/circlesize.js
+++ Javascript/circlesize.js
@@ -2,13 +2,13 @@ var h = 100
 
 var w = 295
 
-var xspa = 30
+// var xspa = 30
 
 
 var svg = d3.select('body').append('svg').attr('width', w).attr('height', h).attr('class','radSol');
 // look at chp 7 of vizualizing data
 
-var circs = [0,1,2,3,4,14,28]
+var circs = [0,1,2,2,2,3,4,14,28,28,28,28,28,28,28,28,28,28]
 
 var rscale = d3.scale.linear()
 
@@ -30,11 +30,11 @@ svg.selectAll('.circles').data(circs)
 svg.selectAll('text')
     .data(circs).enter().append('text')
     .text(function(d){
-        console.log(this)
+        console.log("one---text", this)
         return Math.round((Math.pow(d,2))*Math.PI)
 
     })
-    .attr('font-size', 10)
+    .attr('font-size', 20)
     .attr('fill', 'pink')
     .attr('x', function(d,i){
         return 20 +40*i
@@ -124,6 +124,47 @@ threes.selectAll('text')
     })
     .attr('font-size', 10)
     .attr('fill', 'brown')
+    .attr('x', function(d,i){
+        return 20 +40*i
+    })
+    .attr('y', h/4)
+    .attr('text-anchor', 'middle')
+
+
+d3.select('body').append('html:br')
+
+var four = d3.select('body').append('svg').attr('width', 600).attr('height', h).attr('class','radLin');
+
+var rscale = d3.scale.linear()
+    .domain([0,5])
+    .range([3,10])
+
+
+four.selectAll('.circles').data(circs)
+    .enter()
+    .append('circle')
+
+    .attr('r', function(d){
+        return rscale(d);
+    })
+    .attr('class','circles')
+    .attr('cy', h/2)
+    .attr('cx',function(d,i){
+        console.log("D and I:::", d, i);
+        return 20 + 20 * d
+    })
+    .attr('fill', '#87CEFA')
+
+four.selectAll('text')
+    .data(circs).enter().append('text')
+    .text(function(d){
+        console.log(this);
+        var circRad = rscale(d);
+        return Math.round((circRad * circRad)*Math.PI)
+
+    })
+    .attr('font-size', 10)
+    .attr('fill', 'blue')
     .attr('x', function(d,i){
         return 20 +40*i
     })
